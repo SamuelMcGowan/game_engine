22c522f54ec28525f715a90135d5ae7ee59001a3
squoop <the.sam.mcg@protonmail.com>
2022-10-13 18:41:31 +0100

    Move `EntityMut` into its own module.

--- ecs/src/lib.rs
+++ ecs/src/lib.rs
@@ -1,13 +1,15 @@
+pub mod entity_mut;
 pub mod query;
 pub mod storage;
 pub mod system;
 pub mod world;
 
 pub mod prelude {
+    pub use entity_mut::EntityMut;
     pub use query::*;
     pub use storage::components::*;
     pub use storage::entities::*;
-    pub use storage::{BorrowError, BorrowResult, EntityMut, Iter, IterMut};
+    pub use storage::{BorrowError, BorrowResult, Iter, IterMut};
     pub use system::*;
     pub use world::*;
 
